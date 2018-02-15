17f1fd1a757b2d91a3348abb34fa92602022da0f
Jelle van Assema <jelle.van.assema@gmail.com>
2018-02-15 12:06:50 +0000

    hello simplified

--- cs50/2017/x/hello/check50/__init__.py
+++ cs50/2017/x/hello/check50/__init__.py
@@ -2,25 +2,23 @@ import re
 
 from check50 import *
 
-class Hello(Checks):
+@check()
+def exists(check):
+    """hello.c exists."""
+    check.require("hello.c")
 
-    @check()
-    def exists(self):
-        """hello.c exists."""
-        self.require("hello.c")
+@check("exists")
+def compiles(check):
+    """hello.c compiles."""
+    check.spawn("clang -std=c11 -o hello hello.c").exit(0)
 
-    @check("exists")
-    def compiles(self):
-        """hello.c compiles."""
-        self.spawn("clang -std=c11 -o hello hello.c").exit(0)
-
-    @check("compiles")
-    def prints_hello(self):
-        """prints "hello, world\\n" """
-        expected = "[Hh]ello, world!?\n"
-        actual = self.spawn("./hello").stdout()
-        if not re.match(expected, actual):
-            err = Error(Mismatch("hello, world\n", actual))
-            if re.match(expected[:-1], actual):
-                err.helpers = "Did you forget a newline (\"\\n\") at the end of your printf string?"
-            raise err
+@check("compiles")
+def prints_hello(check):
+    """prints "hello, world\\n" """
+    expected = "[Hh]ello, world!?\n"
+    actual = check.spawn("./hello").stdout()
+    if not re.match(expected, actual):
+        err = Error(Mismatch("hello, world\n", actual))
+        if re.match(expected[:-1], actual):
+            err.helpers = "Did you forget a newline (\"\\n\") at the end of your printf string?"
+        raise err
